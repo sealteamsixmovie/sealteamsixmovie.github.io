570c3e1ff1a5d1b1611065528e8e8219a89bab43
Daniel Dover <dannydfpv@gmail.com>
2019-08-08 16:41:52 -0500

    initial commit

---
+++ js/app.js
@@ -0,0 +1,74 @@
+var controller = new ScrollMagic.Controller();
+ 
+new ScrollMagic.Scene({
+        triggerElement: "#slidein2",
+        triggerHook: "onLeave",
+    })
+    .setPin("#slidein2")
+    //.addIndicators() // add indicators (requires plugin)
+    .addTo(controller);
+
+	var fromLeftTimeline = new TimelineMax();
+	var fromLeftFrom = TweenMax.from("#left", 1, {
+    	x: -800
+	});
+	var fromLeftTo = TweenMax.to("#left", 1, {
+    	x: 0
+	});
+	fromLeftTimeline
+    	.add(fromLeftFrom)
+    	.add(fromLeftTo);
+
+new ScrollMagic.Scene({
+        triggerElement: "#slidein2",
+        offset: 200,
+    })
+    .setTween(fromLeftTimeline)
+    .duration(400)
+    //    .reverse(false)
+    //.addIndicators() // add indicators (requires plugin)
+    .addTo(controller);
+
+// Fade in
+var fadeInTimeline = new TimelineMax();
+var fadeInFrom = TweenMax.from("#opacity", 1, {
+    autoAlpha: 0
+});
+var fadeInTo = TweenMax.to("#opacity", 1, {
+    autoAlpha: 1
+});
+fadeInTimeline
+    .add(fadeInFrom)
+    .add(fadeInTo);
+ 
+new ScrollMagic.Scene({
+        triggerElement: "#slidein2",
+        offset: 200,
+    })
+    .setTween(fadeInTimeline)
+    .duration(400)
+    //    .reverse(false)
+    //.addIndicators() // add indicators (requires plugin)
+    .addTo(controller);
+ 
+//Fly in from the left
+var fromBottomTimeline = new TimelineMax();
+var fromBottomFrom = TweenMax.from("#bottom", 1, {
+    y: 300
+});
+var fromBottomTo = TweenMax.to("#bottom", 1, {
+    y: 0
+});
+fromBottomTimeline
+    .add(fromBottomFrom)
+    .add(fromBottomTo);
+ 
+new ScrollMagic.Scene({
+        triggerElement: "#slidein2",
+        offset: 200,
+    })
+    .setTween(fromBottomTimeline)
+    .duration(400)
+    //    .reverse(false)
+    //.addIndicators() // add indicators (requires plugin)
+    .addTo(controller);   
