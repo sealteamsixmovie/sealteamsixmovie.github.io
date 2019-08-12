542d2a2f610b9bbaab7a3c6b51879b28d7eae0dd
Daniel Dover <dannydfpv@gmail.com>
2019-08-12 14:19:25 -0500

    posters added

--- js/app.js
+++ js/app.js
@@ -10,7 +10,7 @@ new ScrollMagic.Scene({
 
 	var fromLeftTimeline = new TimelineMax();
 	var fromLeftFrom = TweenMax.from("#left", 1, {
-    	x: -800
+    	x: -1300
 	});
 	var fromLeftTo = TweenMax.to("#left", 1, {
     	x: 0
@@ -21,10 +21,10 @@ new ScrollMagic.Scene({
 
 new ScrollMagic.Scene({
         triggerElement: "#slidein2",
-        offset: 200,
+        offset: 1100,
     })
     .setTween(fromLeftTimeline)
-    .duration(400)
+    .duration(600)
     //    .reverse(false)
     //.addIndicators() // add indicators (requires plugin)
     .addTo(controller);
@@ -43,10 +43,10 @@ fadeInTimeline
  
 new ScrollMagic.Scene({
         triggerElement: "#slidein2",
-        offset: 200,
+        offset: 500,
     })
     .setTween(fadeInTimeline)
-    .duration(400)
+    .duration(800)
     //    .reverse(false)
     //.addIndicators() // add indicators (requires plugin)
     .addTo(controller);
@@ -72,3 +72,46 @@ new ScrollMagic.Scene({
     //    .reverse(false)
     //.addIndicators() // add indicators (requires plugin)
     .addTo(controller);   
+
+    // Fade in
+var fadeInTimeline = new TimelineMax();
+var fadeInFrom = TweenMax.from("#opacity", 1, {
+    autoAlpha: 0
+});
+var fadeInTo = TweenMax.to("#opacity", 1, {
+    autoAlpha: 1
+});
+fadeInTimeline
+    .add(fadeInFrom)
+    .add(fadeInTo);
+ 
+new ScrollMagic.Scene({
+        triggerElement: "#slidein2",
+        offset: 300,
+    })
+    .setTween(fadeInTimeline)
+    .duration(800)
+    //    .reverse(false)
+    //.addIndicators() // add indicators (requires plugin)
+    .addTo(controller);
+ 
+
+var fadeInTimeline = new TimelineMax();
+var fadeInFrom = TweenMax.from("#opacity2", 1, {
+    autoAlpha: 0
+});
+var fadeInTo = TweenMax.to("#opacity2", 1, {
+    autoAlpha: 1
+});
+fadeInTimeline
+    .add(fadeInFrom)
+    .add(fadeInTo);
+new ScrollMagic.Scene({
+        triggerElement: "#slidein3",
+        offset: 1500,
+    })
+    .setTween(fadeInTimeline)
+    .duration(1000)
+    //    .reverse(false)
+    //.addIndicators() // add indicators (requires plugin)
+    .addTo(controller);
