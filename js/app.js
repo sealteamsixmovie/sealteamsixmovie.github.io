cb4435b5c7a6b4d5e468c590b9e1da0cc9b77d6f
Daniel Dover <dannydfpv@gmail.com>
2019-08-15 00:00:28 -0500

    ScrollMagic triggers fixed

--- js/app.js
+++ js/app.js
@@ -1,119 +1,32 @@
-var controller = new ScrollMagic.Controller();
- 
-new ScrollMagic.Scene({
-        triggerElement: "#slidein2",
-        triggerHook: "onLeave",
-    })
-    .setPin("#slidein2")
-    //.addIndicators() // add indicators (requires plugin)
-    .addTo(controller);
+    
+    // init controller
+    var controller = new ScrollMagic.Controller();
 
-	var fromLeftTimeline = new TimelineMax();
-	var fromLeftFrom = TweenMax.from("#left", 1, {
-    	x: -1300
-	});
-	var fromLeftTo = TweenMax.to("#left", 1, {
-    	x: 0
-	});
-	fromLeftTimeline
-    	.add(fromLeftFrom)
-    	.add(fromLeftTo);
-
-new ScrollMagic.Scene({
-        triggerElement: "#slidein2",
-        offset: 700,
-    })
-    .setTween(fromLeftTimeline)
-    .duration(600)
-    //    .reverse(false)
-    //.addIndicators() // add indicators (requires plugin)
-    .addTo(controller);
-
-// Fade in
-var fadeInTimeline = new TimelineMax();
-var fadeInFrom = TweenMax.from("#opacity", 1, {
-    autoAlpha: 0
-});
-var fadeInTo = TweenMax.to("#opacity", 1, {
-    autoAlpha: 1
-});
-fadeInTimeline
-    .add(fadeInFrom)
-    .add(fadeInTo);
- 
-new ScrollMagic.Scene({
-        triggerElement: "#slidein2",
-        offset: 300,
-    })
-    .setTween(fadeInTimeline)
-    .duration(800)
-    //    .reverse(false)
-    //.addIndicators() // add indicators (requires plugin)
-    .addTo(controller);
- 
-//Fly in from the left
-var fromBottomTimeline = new TimelineMax();
-var fromBottomFrom = TweenMax.from("#bottom", 1, {
-    y: 300
-});
-var fromBottomTo = TweenMax.to("#bottom", 1, {
-    y: 0
-});
-fromBottomTimeline
-    .add(fromBottomFrom)
-    .add(fromBottomTo);
- 
-new ScrollMagic.Scene({
-        triggerElement: "#slidein2",
-        offset: 800,
-    })
-    .setTween(fromBottomTimeline)
-    .duration(400)
-    //    .reverse(false)
-    //.addIndicators() // add indicators (requires plugin)
-    .addTo(controller);   
+    /*IN AN OCEAN NEAR YOU...*/
+    // build scene
+    new ScrollMagic.Scene({
+                        triggerElement: "#trigger1",
+                        triggerHook: 0.7, // show, when scrolled 20% into view
+                        duration: "60%", // hide 10% before exiting view (60% + 40% from bottom)
+                        offset: 50 // move trigger to center of element
+                    })
+                    .setClassToggle("#reveal1", "visible") // add class to reveal
+                    //.addIndicators() // add indicators (requires plugin)
+                    .addTo(controller);
 
 
 
-    // Fade in
-var fadeInTimeline = new TimelineMax();
-var fadeInFrom = TweenMax.from("#opacity2", 1, {
-    autoAlpha: 0
-});
-var fadeInTo = TweenMax.to("#opacity2", 1, {
-    autoAlpha: 1
-});
-fadeInTimeline
-    .add(fadeInFrom)
-    .add(fadeInTo);
- 
-new ScrollMagic.Scene({
-        triggerElement: "#slidein3",
-        offset: 125,
-    })
-    .setTween(fadeInTimeline)
-    .duration(300)
-    //    .reverse(false)
-    //.addIndicators() // add indicators (requires plugin)
-    .addTo(controller);
- 
+    // init controller
+    var controller = new ScrollMagic.Controller();
+    /*SAMMY LOGLINE*/
+    // build scene
+    new ScrollMagic.Scene({
+                        triggerElement: "#trigger2",
+                        triggerHook: 0.5,
+                        offset: 50, // move trigger to center of element
+                        reverse: true // only do once
+                    })
+                    .setClassToggle("#reveal2", "visible") // add class toggle
+                    //.addIndicators() // add indicators (requires plugin)
+                    .addTo(controller);
 
-var fadeInTimeline = new TimelineMax();
-var fadeInFrom = TweenMax.from("#opacity2", 1, {
-    autoAlpha: 0
-});
-var fadeInTo = TweenMax.to("#opacity2", 1, {
-    autoAlpha: 1
-});
-fadeInTimeline
-    .add(fadeInFrom)
-    .add(fadeInTo);
-new ScrollMagic.Scene({
-        triggerElement: "#slidein3",
-        offset: 550,
-    })
-    .setTween(fadeInTimeline)
-    .duration(1000)
-    //    .reverse(false)
-    //.addIndicators() // add indicators (requires plugin)
-    .addTo(controller);
