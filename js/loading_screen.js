7051d521fc39d0784eeb525f785e58bb30449227
Daniel Dover <dannydfpv@gmail.com>
2019-08-20 18:16:51 -0500

    new preloader added

--- js/loading_screen.js
+++ js/loading_screen.js
@@ -1,4 +1,20 @@
+/*
+
 window.addEventListener("load", function () {
     const loader = document.querySelector(".loader");
     loader.className += " hidden"; // class "loader hidden"
 });
+
+*/
+
+
+
+(function($){
+  'use strict';
+    $(window).on('load', function () {
+        if ($(".pre-loader").length > 0)
+        {
+            $(".pre-loader").fadeOut("slow");
+        }
+    });
+})(jQuery)
