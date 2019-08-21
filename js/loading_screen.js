4fc89a242947b1d888e25078652123d67ac483bd
Daniel Dover <dannydfpv@gmail.com>
2019-08-20 19:09:20 -0500

    intended audience added

--- js/loading_screen.js
+++ js/loading_screen.js
@@ -1,20 +1,7 @@
-/*
-
 window.addEventListener("load", function () {
     const loader = document.querySelector(".loader");
     loader.className += " hidden"; // class "loader hidden"
 });
 
-*/
 
 
-
-(function($){
-  'use strict';
-    $(window).on('load', function () {
-        if ($(".pre-loader").length > 0)
-        {
-            $(".pre-loader").fadeOut("slow");
-        }
-    });
-})(jQuery)
